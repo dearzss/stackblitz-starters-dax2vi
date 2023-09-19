378e89b51cc767c0dc4c512daf668960a19c95e5
dearzss <53515744+dearzss@users.noreply.github.com>
2023-09-19 15:07:21 +0800

    3 files modified, 2 added, 1 deleted

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,26 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
 
-import { App } from './App';
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">
+          #VanLife
+        </Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-const root = createRoot(document.getElementById('app'));
-
-root.render(
-  <StrictMode>
-    <App name="StackBlitz" />
-  </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
